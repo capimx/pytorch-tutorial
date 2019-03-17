3506f366f8775fe5c2f94647b25f000a59fa14d6
capimx <maucabamtz@gmail.com>
2019-03-17 13:34:02 +0000

    Have the two models for comparison

--- tutorials/03-advanced/image_captioning/model_baseline.py
+++ tutorials/03-advanced/image_captioning/model_baseline.py
@@ -1,12 +1,10 @@
 import torch
-import gensim
-import numpy as np
 import torch.nn as nn
 import torchvision.models as models
-from bert_embedding import BertEmbedding
 from torch.nn.utils.rnn import pack_padded_sequence
 
-class EncoderCNN(nn.Module):
+
+class EncoderCNNBaseline(nn.Module):
     def __init__(self, embed_size):
         """Load the pretrained ResNet-152 and replace top fc layer."""
         super(EncoderCNN, self).__init__()
@@ -25,35 +23,13 @@ class EncoderCNN(nn.Module):
         return features
 
 
-class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab, num_layers, max_seq_length=20):
+class DecoderRNNBaseline(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
         """Set the hyper-parameters and build the layers."""
         super(DecoderRNN, self).__init__()
-        Bert_file = "bert-base-uncased.30522.768d.vec"
-        print("M1")
-        Lookup = gensim.models.KeyedVectors.load_word2vec_format(Bert_file, binary=False)
-        bert_embedding = BertEmbedding()
-        Embed = np.zeros((len(vocab), embed_size))
-        print("M2")
-        Embed[vocab('<pad>'),:] = np.random.normal(0, 1, embed_size)
-        Embed[vocab('<start>'),:] = np.random.normal(0, 1, embed_size)
-        Embed[vocab('<end>'),:] = np.random.normal(0, 1, embed_size)
-        Embed[vocab('<unk>'),:] = np.random.normal(0, 1, embed_size)
-        print("M3")
-        for word in vocab.__keys__()[4:]:
-            try:
-                Embed[vocab(word),:] = Lookup[word]
-            except:
-                bert_word = word
-                token = bert_word.split('\n')                
-                pred = bert_embedding(token)
-                Embed[vocab(word),:] = pred[0][1][0]
-        
-        print("M4")
-        self.embed = nn.Embedding(len(vocab), embed_size)
-        self.embed.weight.data.copy_(torch.FloatTensor(Embed))
+        self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
-        self.linear = nn.Linear(hidden_size, len(vocab))
+        self.linear = nn.Linear(hidden_size, vocab_size)
         self.max_seg_length = max_seq_length
         
     def forward(self, features, captions, lengths):
@@ -71,7 +47,7 @@ class DecoderRNN(nn.Module):
         inputs = features.unsqueeze(1)
         for i in range(self.max_seg_length):
             hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
-            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, len(vocab))
+            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
             _, predicted = outputs.max(1)                        # predicted: (batch_size)
             sampled_ids.append(predicted)
             inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
